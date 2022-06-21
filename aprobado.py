c772668ecded73670068f8c8cf9aa96adeb7481a
Augusto1991 <augusto.mastroverti@gmail.com>
2022-06-21 19:59:18 -0300

    mi primer commit

---
+++ aprobado.py
@@ -0,0 +1,10 @@
+#Crear un script llamado aprobado.py que realice lo siguiente:
+#Debe tomar 2 argumentos, ambos números enteros del 0 al 10, sino, mostrar un error.
+#Si ambos valores son mayores o igual a 7 devolver imprimir “Promocionado”
+#Si ambos valores son mayor o igual a 4 imprimir “Aprobado, debe rendir final”
+import sys
+if len (sys.argv) !=2:
+    print("solo 2 argumentos")
+else
+    for i in range (int(sys.argv[2]))
+        print(sys.argv[1])
